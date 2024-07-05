f758d1298791e500dc69e01a4a9f10a8952b70c9
cy <qu28@qq.com>
2024-07-05 11:28:32 +0800

    添加项目文件。

---
+++ jenkinsxUnit/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace jenkinsxUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(false);
+        }
+    }
+}
